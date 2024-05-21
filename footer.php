3149d21ca848e5c331d099faf520d85c3da7adfa
tamz-z <150661887+tamz-z@users.noreply.github.com>
2024-05-21 10:46:03 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<footer>
+</footer>
+</body>
+
+</html>

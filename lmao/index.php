a0fc84bebe3f753ec5d0b0d2223005eb6788ff5a
mikeybtw <mikebtw13@outlook.com>
2024-05-21 10:40:45 +0200

    homepage + afbeelding

--- lmao/index.php
+++ lmao/index.php
@@ -16,18 +16,22 @@
             <a href="index.php">Home</a>
             <a href="over.php">Over Ons</a>
             <a href="contact.php">Contact</a>
-            <a href="faq.php">Veelgestelde Vragen<a>
+            <a href="faq.php">Veelgestelde Vragen</a>
         </nav>
     </header>
+
+
     <img src="assets/beachvilla.jpg">
     <div class="header-text">
         <h2>Grab the deal & make it real!</h2>
         <h1>Nieuw bij Agent Vista</h1>
     </div>
-
-
-
-
+    <div class="vakantie">
+        <h2>Vakantie Inspiratie</h2>
+    </div>
+    <img src="assets/retangle.png" class="retangle">
+    <img src="assets/yacht.jpg" class="yacht">
+    <img src="assets/palmbomen.jpg" class="palm">
 </body>
 
 </html>

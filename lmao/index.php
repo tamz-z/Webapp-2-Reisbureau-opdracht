f294ee888054a14c31b7e68f09ba97c3748295a2
mikeybtw <mikebtw13@outlook.com>
2024-05-28 10:45:33 +0200

    over ons pagina + contact

--- lmao/index.php
+++ lmao/index.php
@@ -1,30 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Reisbureau Front Page</title>
-</head>
+<?php
+include "header.php";
+?>
 
 <body>
-    <header class="header">
-        <nav class="navbar">
-            <img src="assets/vistalogo.png" class="logo">
-            <a href="index.php">Home</a>
-            <a href="over.php">Over Ons</a>
-            <a href="contact.php">Contact</a>
-            <a href="faq.php">Veelgestelde Vragen</a>
-        </nav>
-    </header>
-
-
-    <img src="assets/beachvilla.jpg">
+    <img src="assets/beachvilla.jpg" alt="Beach Villa">
     <div class="header-text">
         <h2>Grab the deal & make it real!</h2>
-        <h1>Nieuw bij Agent Vista</h1>
     </div>
     <div class="vakantie">
         <h2>Vakantie Inspiratie</h2>
@@ -32,6 +13,20 @@
     <img src="assets/retangle.png" class="retangle">
     <img src="assets/yacht.jpg" class="yacht">
     <img src="assets/palmbomen.jpg" class="palm">
+    <img src="assets/manunderwater.png" class="water">
+    <img src="assets/bali.jpg" class="bali">
+    <img src="assets/cocktail.jpg" class="cocktail">
+    <img src="assets/retangle.png" class="retangle2">
+    <div class="welcome">
+        <h2>Welkom bij Onze</h2>
+        <h2>Services</h2>
+    </div>
+    <img src="assets/water.png" class="water2">
+    <img src="assets/mountains.png" class="berg">
+    <img src="assets/manhattan.png" class="ny">
+
+
+    </div>
 </body>
 
 </html>

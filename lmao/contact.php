20dbea6636668ae06dd5f7dd4775ace6fc17fd6a
tamz-z <150661887+tamz-z@users.noreply.github.com>
2024-05-30 13:41:28 +0200

    Update contact.php

--- lmao/contact.php
+++ lmao/contact.php
@@ -2,14 +2,39 @@
 include "header.php";
 ?>
 
+
 <body>
-    <img src="assets/contact.jpg">
+    <img style="width: 100%" src="assets/contact.jpg">
     <div class="contact">
-        <h1>NEEM CONTACT MET ONS OP</h1>
+        <h5 >NEEM CONTACT MET ONS OP</h5>
     </div>
+    <div class="Contactblock">
+        <div class="Contact1stehelft">
+            <h4 style="color: #368296;">
+                AgentVista
+            </h4>
+            <h3 style="color: #66b0bd;">
+                is jouw betrouwbare reisbureau, gespecialiseerd in het bieden van uitzonderlijke reiservaringen.
+                Ons team van experts staat klaar om je te ondersteunen met advies, reisplannen en antwoorden op al je vragen.
+                We waarderen je feedback en streven ernaar om onze diensten continu te verbeteren.
+                Heb je een probleem of heb je informatie nodig? Laat ons je helpen.
+                Heb je tips, trucs of klachten? Voel je vrij om contact met ons op te nemen.
+            </h3>
+        </div>
 
-
-
+        <div class="Contact2stehelft">
+            <div>
+                <h2>Vul je voornaam in</h2>
+                <input class="Balkcontact" type="text" name="Naam"  required>
+                <h2>Vul je achternaam in</h2>
+                <input class="Balkcontact" type="text" name="Achternaam"  required>
+            </div>
+            <h2>Vul je e-mailadres in</h2>
+            <input class="Balkcontact" type="email" name="email"  required>
+            <h2>Vul je bericht in</h2>
+            <input class="Balkcontact" style=" height: 200px" type="text" name="Bericht"  required>
+        </div>
+    </div>
 
 
 </body>
